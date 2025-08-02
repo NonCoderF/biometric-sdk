2e3319900cef9710bb09ccbd9a327e2cde5f64b3
Nizamuddin Ahmed <sallyinfo365@gmail.com>
2025-08-02 15:41:30 +0530

    Finalize v1.0.0 release with polished README

--- biometric/src/main/java/com/sparkstudios/biometric/AppVisibilityTracker.kt
+++ biometric/src/main/java/com/sparkstudios/biometric/AppVisibilityTracker.kt
@@ -3,7 +3,7 @@ package com.sparkstudios.biometric
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
-public object AppVisibilityTracker : DefaultLifecycleObserver {
+object AppVisibilityTracker : DefaultLifecycleObserver {
 
     override fun onStop(owner: LifecycleOwner) {
         super.onPause(owner)
